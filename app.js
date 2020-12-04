e552da8f2b9f32f995bda657364608564e595881
ERICH SAUER <erich.sauer@gmail.com>
2020-12-03 17:10:27 -0800

    added feature to change name tag color!

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const name = document.getElementById('name');
 const input = document.getElementById('input');
 const button = document.getElementById('button');
+const cyan = document.getElementById('button-cyan');
+const orange = document.getElementById('button-orange');
+const pink = document.getElementById('button-pink');
+const backgroundColor = document.getElementById('header');
 
 button.addEventListener('click', () => {
     name.textContent = input.value
     input.value = ''
+})
+cyan.addEventListener('click', () => {
+    backgroundColor.style.background = "cyan"
+})
+orange.addEventListener('click', () => {
+    backgroundColor.style.background = "orange"
+})
+pink.addEventListener('click', () => {
+    backgroundColor.style.background = "pink"
 })

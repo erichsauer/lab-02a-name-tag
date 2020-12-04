3f1503eaf954d6f0b62de1c55cdcdfb43f52a46f
ERICH SAUER <erich.sauer@gmail.com>
2020-12-03 21:27:02 -0800

    added option so that the name tag includes user input for pronouns

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
 const name = document.getElementById('name');
-const input = document.getElementById('input');
+const nameIn = document.getElementById('name-in');
+const prououns = document.getElementById('pronouns');
+const pronounsIn = document.getElementById('pronouns-in');
 const button = document.getElementById('button');
 const cyan = document.getElementById('button-cyan');
 const orange = document.getElementById('button-orange');
 const pink = document.getElementById('button-pink');
 const backgroundColor = document.getElementById('header');
 
+nameIn.addEventListener('click', () => {
+    nameIn.value = ''
+});
+pronounsIn.addEventListener('click', () => {
+    pronounsIn.value = ''
+});
 button.addEventListener('click', () => {
-    name.textContent = input.value
-    input.value = ''
-})
+    name.textContent = nameIn.value
+    name.value = ''
+});
+button.addEventListener('click', () => {
+    pronouns.textContent = pronounsIn.value
+    pronouns.value = ''
+});
 cyan.addEventListener('click', () => {
     backgroundColor.style.background = "cyan"
-})
+});
 orange.addEventListener('click', () => {
     backgroundColor.style.background = "orange"
-})
+});
 pink.addEventListener('click', () => {
-    backgroundColor.style.background = "pink"
-})
+    backgroundColor.style.backgroundImage = "pink"
+});
